62981f5d9950c134accec9d91a505e45d7d2c2d7
Yudi Fathurrahman <yfathurrahman@gmail.com>
2021-10-26 13:11:25 +0700

    cors

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require('path');
 const env = require('dotenv');
 const methodOverride = require('method-override');
+const cors = require('cors');
+
 env.config();
 const routes = require('./routes/routes');
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
